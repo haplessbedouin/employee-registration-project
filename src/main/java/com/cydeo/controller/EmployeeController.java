034d9da31400e4cdf0d3d1b4f411e2f3e4b02139
Ahmet Tugrul <ahmedsaid0173@gmail.com>
2023-02-27 12:03:16 -0600

    Employee register page and list table completed

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,15 +1,47 @@
 package com.cydeo.controller;
 
+import com.cydeo.bootstrap.DataGenerator;
+import com.cydeo.model.Employee;
+import com.cydeo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
     @GetMapping("/register")
-    public String createEmployee(){
+    public String createEmployee(Model model){
+
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("stateList", DataGenerator.getAllStates());
+
         return "/employee/employee-create";
+    }
+    @PostMapping("/insert")
+    public String insertEmployee(@ModelAttribute("employee") Employee employee){
+
+        employeeService.saveEmployee(employee);
+        return "redirect:/employee/list";
+
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model){
+
+        model.addAttribute("employeeList", employeeService.readAllEmployees());
+        return "/employee/employee-list";
+
     }
 
 }
--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,6 +3,7 @@ package com.cydeo.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -13,6 +14,9 @@ public class Employee {
 
     private String firstName;
     private String lastName;
+
+    // Thymeleaf accepts yyyy-MM-dd, but LocalDate accepts mm-dd-yyyy so we can use @DateTimeFormat annotation
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
     private String email;
     private String password;

970a068a4243b48db52bbbd60d95a36eee292d60
Ahmet Tugrul <ahmedsaid0173@gmail.com>
2023-02-27 20:19:45 -0600

    Some validations added

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -3,8 +3,10 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.model.Employee;
 import com.cydeo.service.EmployeeService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +31,12 @@ public class EmployeeController {
         return "/employee/employee-create";
     }
     @PostMapping("/insert")
-    public String insertEmployee(@ModelAttribute("employee") Employee employee){
+    public String insertEmployee(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult, Model model){
+
+        if (bindingResult.hasErrors()){
+            model.addAttribute("stateList", DataGenerator.getAllStates());
+            return "employee/employee-create";
+        }
 
         employeeService.saveEmployee(employee);
         return "redirect:/employee/list";
--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -1,5 +1,7 @@
 package com.cydeo.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,10 +14,20 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class Employee {
 
+    // @NotNull --> Field shouldn't be null
+    // @NotEmpty --> Field shouldn't be ""
+    // @NotBlank --> Field shouldn't be "            "
+
+    // @NotNull --> @NotNull
+    // @NotEmpty --> @NotNull + NotEmpty
+    // @NotBlank --> @NotNull + NotEmpty + NotBlank
+
+    @NotBlank
+    @Size(max = 12, min = 2)
     private String firstName;
     private String lastName;
 
-    // Thymeleaf accepts yyyy-MM-dd, but LocalDate accepts mm-dd-yyyy so we can use @DateTimeFormat annotation
+    // Thymeleaf accepts yyyy-MM-dd, but LocalDate accepts mm-dd-yyyy, so we can use @DateTimeFormat annotation
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
     private String email;
